0ab1acb66431a4e991558f49b69c2679f7963ce8
umitkose <umitkose@turksat.com.tr>
2022-01-24 10:10:47 +0300

    add file read and write example

--- src/com/umiitkose/java/basic/file/DosyaOkumaYazma.java
+++ src/com/umiitkose/java/basic/file/DosyaOkumaYazma.java
@@ -1,52 +1,54 @@
 package com.umiitkose.java.basic.file;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
 
 public class DosyaOkumaYazma {
 
+
+    public static final Logger logger = Logger.getLogger(DosyaOkumaYazma.class.getName());
+
     public static void main(String[] args) {
 
-        DosyaOkumaYazma dosyaOkumaKapatma = new DosyaOkumaYazma();
-        dosyaOkumaKapatma.writeFile(getFile());
-        dosyaOkumaKapatma.readFile(getFile());
+        DosyaOkumaYazma DosyaOkumaYazma = new DosyaOkumaYazma();
+        DosyaOkumaYazma.writeFile(getFile());
+        DosyaOkumaYazma.readFile(getFile());
 
     }
 
     private static File getFile() {
-        String filePath = "file_path -> example --> /Users/umitkose/Documents/TCKimlikNoTest.txt";
+        String filePath = "file_path";
         return new File(filePath);
     }
 
     private void readFile(File file) {
         // if(file.canRead() && file.canExecute() && file.isFile()){ //--> Farklı kontrolleri var.
-        try {
-            InputStream in = new FileInputStream(file);
-            System.out.println("File Opened");
+        try (InputStream in = new FileInputStream(file)) {
+            logger.info("File Opened");
             int ch = 0;
             while (ch != -1) {
                 ch = in.read();
                 char karakter = (char) ch;
-                System.out.print(karakter);
+                System.out.println(karakter);
             }
-            System.out.println();
-            in.close();
-            System.out.println("File Closed");
+
+            logger.info("File Closed");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // }
     }
 
     private void writeFile(File file) {
 
-        try {
-            OutputStream in = new FileOutputStream(file);
-            System.out.println("File Opened");
+        try (OutputStream in = new FileOutputStream(file)) {
+
+            logger.info("File Opened");
             String write = "Ümit KÖSE";
-            byte[] bytes = write.getBytes("US-ASCII");
+            byte[] bytes = write.getBytes(StandardCharsets.US_ASCII);
             in.write(bytes);
-            in.close();
-            System.out.println("File Closed");
+
+            logger.info("File Closed");
         } catch (IOException e) {
             e.printStackTrace();
         }
